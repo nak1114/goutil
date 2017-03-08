d912438a4c2acbc24d94b32126495f06d1fe5daf
nak1114 <naktak1114@gmail.com>
2017-03-08 23:10:41 +0900

    add Neq and StubIO

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ go utility
 File/Directry copy
 
 refer: https://gist.github.com/magicianzrh/d29d08f4fdd8b3c2f8ee
+
+##assert
+
+Utility for test assertion
+
--- assert/main.go
+++ assert/main.go
@@ -56,3 +56,38 @@ func Eq(actual interface{}, expected interface{}) {
 		t.Errorf("\ngot %v\nwant %v", actual, expected)
 	}
 }
+
+// Neq is general equal assertion
+func Neq(actual interface{}, expected interface{}) {
+	count++
+	if actual == expected {
+		printMessage()
+		t.Errorf("\ngot %v\nwant %v", actual, expected)
+	}
+}
+
+// StubIO stubs Stdin Stdout Stderr in 'fn'.return Stdout and Stderr
+func StubIO(inbuf string, fn func()) (string, string) {
+	inr, inw, _ := os.Pipe()
+	outr, outw, _ := os.Pipe()
+	errr, errw, _ := os.Pipe()
+	orgStdin := os.Stdin
+	orgStdout := os.Stdout
+	orgStderr := os.Stderr
+	inw.Write([]byte(inbuf))
+	inw.Close()
+	os.Stdin = inr
+	os.Stdout = outw
+	os.Stderr = errw
+	fn()
+	os.Stdin = orgStdin
+	os.Stdout = orgStdout
+	os.Stderr = orgStderr
+	outw.Close()
+	outbuf, _ := ioutil.ReadAll(outr)
+	errw.Close()
+	errbuf, _ := ioutil.ReadAll(errr)
+
+	return string(outbuf), string(errbuf)
+
+}
