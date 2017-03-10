d6ffe6eba62a99716e8f8d551d4345e87d24750b
nak1114 <naktak1114@gmail.com>
2017-03-10 23:45:24 +0900

    Use reflect.DeepEqual for Equal assertion
    Add submessage

--- assert/main.go
+++ assert/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
+	"regexp"
 	"testing"
+
+	"github.com/kr/pretty"
 )
 
 var t = (*testing.T)(nil)
 var message = "initial"
-var submessage = []string{}
+var submessage = [10]string{}
+var subdepth = 0
 var count = 0
 
 // Set test title
@@ -19,8 +24,22 @@ func Set(tt *testing.T, mes string) {
 	t = tt
 }
 
+// Sub set sub test title
+func Sub(n int, mes string) {
+	if n > 0 && n < 10 {
+		submessage[n-1] = mes
+		count = 0
+		subdepth = n
+	}
+}
+
 func printMessage() {
 	fmt.Println(message, count)
+	str := "  "
+	for i := 0; i < subdepth; i++ {
+		fmt.Println(str, submessage[i])
+		str += "  "
+	}
 }
 
 // FileContent is expect file content.
@@ -51,7 +70,18 @@ func FileContent(fname string, content string) {
 // Eq is general equal assertion
 func Eq(actual interface{}, expected interface{}) {
 	count++
-	if actual != expected {
+	if !reflect.DeepEqual(actual, expected) {
+		printMessage()
+		pretty.Printf("--- actual :\n%# v\n\n", actual)
+		pretty.Printf("--- expect :\n%# v\n\n", expected)
+		t.Errorf("Not equal\n")
+	}
+}
+
+// EqRegexp is general equal assertion
+func EqRegexp(actual string, expected string) {
+	count++
+	if !regexp.MustCompile(expected).MatchString(actual) {
 		printMessage()
 		t.Errorf("\ngot %v\nwant %v", actual, expected)
 	}
